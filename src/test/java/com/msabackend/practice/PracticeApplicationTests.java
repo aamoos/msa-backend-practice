af0fee03cc71c88cd458738f548768f3cde86335
aamoos <aamoos@naver.com>
2024-01-22 20:59:46 +0900

    Initial commit

---
+++ src/test/java/com/msabackend/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msabackend.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

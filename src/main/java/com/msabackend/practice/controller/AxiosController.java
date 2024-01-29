fc712d44a440f3d0a7b43ea28730f2aa84061917
aamoos <aamoos@naver.com>
2024-01-29 17:15:12 +0900

    포트변경, api url 변경

--- src/main/java/com/msabackend/practice/controller/AxiosController.java
+++ src/main/java/com/msabackend/practice/controller/AxiosController.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/api/axios")
+@RequestMapping(value = "/api/v1/axios")
 @RequiredArgsConstructor
 public class AxiosController {
     private final AxiosService axiosService;
